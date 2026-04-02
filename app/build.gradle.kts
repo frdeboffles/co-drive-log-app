c93da4ba750daae6efbaa1bb22acffd798234809
frd <francois-regis.deboffles@vaisala.com>
2026-04-01 18:41:15 -0600

    release: bump version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.codrivelog.app"
         minSdk = 34
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

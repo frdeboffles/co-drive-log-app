b9c0df8472712d098dfdae5f696c2baac3786db1
frd <francois-regis.deboffles@vaisala.com>
2026-04-05 11:01:48 -0600

    build: upgrade AGP and Gradle wrapper for 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.codrivelog.app"
         minSdk = 34
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
